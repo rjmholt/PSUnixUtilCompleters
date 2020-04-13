d66935595352e0e27638d369147d7c25327469ab
Robert Holt <rjmholt_msft@outlook.com>
2020-04-13 11:21:35 -0700

    Add deprecation notice, remove license acceptance

--- OnStart.ps1
+++ OnStart.ps1
@@ -1,5 +1,19 @@
 # Informative messages to let users know that completers have been registered
 
+Write-Warning @"
+=== DEPRECATION NOTICE ===
+
+This module is now available as Microsoft.PowerShell.UnixCompleters.
+The PSUnixUtilCompleters module package is now deprecated in favour of the new package,
+and will not be receiving updates.
+
+To update to the new module:
+
+    Install-Module Microsoft.PowerShell.UnixCompleters
+
+Don't forget to uninstall this module in the next PowerShell session.
+"@
+
 Write-Verbose "Registering UNIX native util completers"
 
 # We don't have access to the module at load time, since loading occurs last
--- PSUnixUtilCompleters.psd1
+++ PSUnixUtilCompleters.psd1
@@ -12,7 +12,7 @@
 RootModule = 'PSUnixUtilCompleters.dll'
 
 # Version number of this module.
-ModuleVersion = '0.1.0'
+ModuleVersion = '0.1.1'
 
 # Supported PSEditions
 CompatiblePSEditions = 'Core'
@@ -117,7 +117,7 @@ PrivateData = @{
         # Prerelease = ''
 
         # Flag to indicate whether the module requires explicit user acceptance for install/update/save
-        RequireLicenseAcceptance = $true
+        RequireLicenseAcceptance = $false
 
         # External dependent modules of this module
         # ExternalModuleDependencies = @()
